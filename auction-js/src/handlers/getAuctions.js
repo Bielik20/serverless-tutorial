26f1f0779f03326f7dfd0595cfbaba300bca0250
Damian Bielecki <bielik20@gmail.com>
2020-10-31 11:17:47 +0100

    Add commonMiddleware

--- auction-js/src/handlers/getAuctions.js
+++ auction-js/src/handlers/getAuctions.js
@@ -1,9 +1,6 @@
 import createError from 'http-errors';
 import AWS from 'aws-sdk';
-import httpJsonBodyParser from '@middy/http-json-body-parser';
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
-import middy from '@middy/core';
+import commonMiddleware from '../lib/commonMiddleware';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -29,7 +26,4 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = middy(getAuctions)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = commonMiddleware(getAuctions);
