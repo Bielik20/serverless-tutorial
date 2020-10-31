1154a0756773c1d19572e11fcc04ba655e7bc6cf
Damian Bielecki <bielik20@gmail.com>
2020-10-31 14:03:58 +0100

    Add closeAuction

--- auction-js/src/handlers/processAuctions.js
+++ auction-js/src/handlers/processAuctions.js
@@ -1,8 +1,20 @@
-import { getEndedAuctions } from "../lib/getEndedAuctions";
+import createError from 'http-errors';
+import { closeAuction } from '../lib/closeAuction';
+import { getEndedAuctions } from '../lib/getEndedAuctions';
 
 async function processAuctions(event, context) {
-  const auctionsToClose = await getEndedAuctions();
-  console.log('processing auctions!', auctionsToClose);
+  try {
+    const auctionsToClose = await getEndedAuctions();
+
+    await Promise.all(
+      auctionsToClose.map((auction) => closeAuction(auction)),
+    );
+
+    return { closed: auctionsToClose.length };
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 }
 
 export const handler = processAuctions;
