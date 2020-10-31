2e5ac6becd9d2a145ed50c7dfd9db10898c983b4
Damian Bielecki <bielik20@gmail.com>
2020-10-31 13:37:10 +0100

    Add auctionsQuery

--- auction-js/src/handlers/processAuctions.js
+++ auction-js/src/handlers/processAuctions.js
@@ -1,5 +1,8 @@
+import { getEndedAuctions } from "../lib/getEndedAuctions";
+
 async function processAuctions(event, context) {
-  console.log('processing auctions!');
+  const auctionsToClose = await getEndedAuctions();
+  console.log('processing auctions!', auctionsToClose);
 }
 
 export const handler = processAuctions;
