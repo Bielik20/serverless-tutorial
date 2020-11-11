cbcae986a38c862e954de36e01af792502e84007
Damian Bielecki <bielik20@gmail.com>
2020-11-11 11:12:29 +0100

    Complete uploading picture

--- auction-js/src/handlers/uploadAuctionPicture.js
+++ auction-js/src/handlers/uploadAuctionPicture.js
@@ -2,6 +2,7 @@ import httpErrorHandler from '@middy/http-error-handler';
 import middy from '@middy/core';
 import { getAuctionById } from './getAuction';
 import { uploadPictureToS3 } from '../lib/updatedPictureToS3';
+import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl';
 import createHttpError from 'http-errors';
 
 async function uploadAuctionPicture(event) {
@@ -10,9 +11,12 @@ async function uploadAuctionPicture(event) {
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
   const buffer = Buffer.from(base64, 'base64');
 
+  let updatedAuction;
+
   try {
-    const result = await uploadPictureToS3(auction.id + '.jpg', buffer);
-    console.log(result);
+    const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer);
+
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
   } catch (error) {
     console.error(error);
     throw new createHttpError.InternalServerError(error);
@@ -20,8 +24,8 @@ async function uploadAuctionPicture(event) {
 
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify(updatedAuction),
   };
 }
 
-export const handler = middy(uploadAuctionPicture).use(httpErrorHandler);
+export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
